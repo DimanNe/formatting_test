18dc0ffbdf2e43ff0290e720836b4b92661c5316
DimanNe <dimanne@gmail.com>
2019-12-15 18:22:46 +0000

    test

--- main.cpp
+++ main.cpp
@@ -13,11 +13,8 @@ int main() {
    auto start = std::chrono::system_clock::now();
    const long result = calc_fibonacci(43);
    auto finish = std::chrono::system_clock::now();
-   auto duration = std::chrono::duration_cast<
-       std::chrono::milliseconds>(finish - start);
-   std::time_t start_time =
-       std::chrono::system_clock::to_time_t(
-           start);
+   auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
+   std::time_t start_time = std::chrono::system_clock::to_time_t(start);
    std::cout << std::ctime(&start_time)
              << std::endl;
    std::cout << duration.count() << "ms."

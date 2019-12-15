46628ded8f1d6e7bde81c8fcf5fc2584a87f86c6
DimanNe <dimanne@gmail.com>
2019-12-15 16:50:53 +0000

    initial

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <chrono>
+#include <ctime>
+#include <iostream>
+#include <util/compiler.h>
+
+long calc_fibonacci(long n) {
+   if(n < 2)
+      return 1;
+   return calc_fibonacci(n - 1) + calc_fibonacci(n - 2);
+}
+
+int main() {
+   auto       start                = std::chrono::system_clock::now();
+   const long result               = calc_fibonacci(43);
+   auto       finish               = std::chrono::system_clock::now();
+   auto       duration             = std::chrono::duration_cast<std::chrono::milliseconds>(finish - start);
+   MBUNUSED std::time_t start_time = std::chrono::system_clock::to_time_t(start);
+   std::cout << std::ctime(&start_time) << std::endl;
+   std::cout << duration.count() << "ms."
+             << " Result: " << result << std::endl;
+   return 0;
+}
